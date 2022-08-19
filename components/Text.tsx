3783c523374239d25de542c8584585e60c06ff88
Jaegar Sarauer <jaegars@hotmail.com>
2022-08-19 14:04:02 -0700

    Variants moved to universal file.

--- components/Text.tsx
+++ components/Text.tsx
@@ -1,15 +1,9 @@
 import { styled } from "../styles/theme";
+import { center, textCenter } from "../styles/variants";
 
 export const Text = styled("p", {
   margin: 0,
   variants: {
-    center: {
-      true: {
-        position: "absolute",
-        top: "50%",
-        left: "50%",
-        transform: "translate(-50%, -50%)",
-      },
-    },
-  },
+    center, textCenter
+  }
 });
---
+++ styles/variants.tsx
@@ -0,0 +1,14 @@
+export const center = {
+  true: {
+    position: "absolute",
+    top: "50%",
+    left: "50%",
+    transform: "translate(-50%, -50%)",
+  },
+};
+
+export const textCenter = {
+  true: {
+    textAlign: "center",
+  },
+};
