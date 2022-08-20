99b5b4bff944366c1fc7a128eab677e241f0e68c
Jaegar Sarauer <jaegars@hotmail.com>
2022-08-19 17:58:43 -0700

    PageTile as a styled component.

--- components/PageTile.tsx
+++ components/PageTile.tsx
@@ -1,19 +1,7 @@
-interface Props {
-  children: JSX.Element;
-}
+import { CSS } from "@stitches/react";
+import { styled } from "../styles/theme";
 
-export const PageTile = ({ children }: Props) => {
-  return (
-    <>
-      <div
-        style={{
-          backgroundColor: "green",
-          width: "100vw",
-          height: "100vh",
-        }}
-      >
-        {children}
-      </div>
-    </>
-  );
-};
+export const PageTile = styled("div", {
+  margin: 0,
+  height: "100vh",
+});
